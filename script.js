d55a7ab534a5209f96dc743c79e6be13be4927bd
VIN07grinder <vihaankrao@gmail.com>
2025-05-01 21:39:35 +0000

    idk at this point bro

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ function loadMovie() {
         alert("Please enter a valid TMDB ID!");
     }
 }
+
+function launchFullscreenMovie() {
+    const tmdbId = document.getElementById('tmdbId').value;
+    if (tmdbId) {
+        let newWindow = window.open('', '_blank', 'noopener,noreferrer');
+        newWindow.document.write(`
+            <html>
+            <head>
+                <style>
+                    body { margin: 0; overflow: hidden; background-color: black; }
+                    iframe { width: 100vw; height: 100vh; border: none; }
+                </style>
+            </head>
+            <body>
+                <iframe src="https://vidlink.pro/movie/${tmdbId}" allowfullscreen></iframe>
+            </body>
+            </html>
+        `);
+        newWindow.document.close();
+    } else {
+        alert("Please enter a valid TMDB ID!");
+    }
+}
